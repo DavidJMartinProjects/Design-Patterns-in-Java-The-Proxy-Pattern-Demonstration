a541a0d1d1d869465678f664abffef7df4016c8b
David Martin <davidjmartin@hotmail.com>
2016-07-28 12:40:13 +0100

    Update Bank_RealSubject.java

--- DesignPatterns_ProxyPattern/src/Bank_RealSubject.java
+++ DesignPatterns_ProxyPattern/src/Bank_RealSubject.java
@@ -8,11 +8,11 @@ public class Bank_RealSubject implements ATM_Interface {
     int accountIndex;    
     
     public Bank_RealSubject() {
-     accountArray.add(new Account(100, 1000.00, "Dave Martin", "Athlone", "Personal", "Good"));
-     accountArray.add(new Account(101, 1000.00, "Joe Bloggs", "Galway", "Corporate", "Good"));
-     accountArray.add(new Account(102, 1000.00, "Jane Bloggs", "Dublin", "Personal", "Good"));
-     accountArray.add(new Account(103, 1000.00, "John Doe", "Waterford", "Corporate", "Good"));
-     NumOfAccounts = 4;
+	 accountArray.add(new Account(100, 1000.00, "Dave Martin", "Athlone", "Personal", "Good"));
+	 accountArray.add(new Account(101, 1000.00, "Joe Bloggs", "Galway", "Corporate", "Good"));
+	 accountArray.add(new Account(102, 1000.00, "Jane Bloggs", "Dublin", "Personal", "Good"));
+	 accountArray.add(new Account(103, 1000.00, "John Doe", "Waterford", "Corporate", "Good"));
+	 NumOfAccounts = 4;
     }
 	
     public void createAccount(int a, double b, String c, String d, String e, String f) {
@@ -21,14 +21,14 @@ public class Bank_RealSubject implements ATM_Interface {
     }
 
     public void deleteAccount(int anAccNumber) {
-		Iterator<Account> iterator = accountArray.iterator();
+	Iterator<Account> iterator = accountArray.iterator();
 	
-		while(iterator.hasNext()) {
-			if(iterator.next().getAccNo() == (anAccNumber)) {
-				iterator.remove();
-			}
+	while(iterator.hasNext()) {
+		if(iterator.next().getAccNo() == (anAccNumber)) {
+		iterator.remove();
 		}
-		NumOfAccounts--;
+	}
+	NumOfAccounts--;
     }
 
     public void updateAccBalance(int anAccNumber, double newBalance) {
