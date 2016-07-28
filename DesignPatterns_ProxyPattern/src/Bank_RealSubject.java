dfad1e40144d7926a7c6b427d61ada67caaf5a89
David Martin <davidjmartin@hotmail.com>
2016-07-28 12:39:23 +0100

    Update Bank_RealSubject.java

--- DesignPatterns_ProxyPattern/src/Bank_RealSubject.java
+++ DesignPatterns_ProxyPattern/src/Bank_RealSubject.java
@@ -7,22 +7,20 @@ public class Bank_RealSubject implements ATM_Interface {
     private int NumOfAccounts = 0;
     int accountIndex;    
     
-	public Bank_RealSubject() {
-		accountArray.add(new Account(100, 1000.00, "Dave Martin", "Athlone", "Personal", "Good"));
-		accountArray.add(new Account(101, 1000.00, "Joe Bloggs", "Galway", "Corporate", "Good"));
-		accountArray.add(new Account(102, 1000.00, "Jane Bloggs", "Dublin", "Personal", "Good"));
-		accountArray.add(new Account(103, 1000.00, "John Doe", "Waterford", "Corporate", "Good"));
-		NumOfAccounts = 4;
-	}
+    public Bank_RealSubject() {
+     accountArray.add(new Account(100, 1000.00, "Dave Martin", "Athlone", "Personal", "Good"));
+     accountArray.add(new Account(101, 1000.00, "Joe Bloggs", "Galway", "Corporate", "Good"));
+     accountArray.add(new Account(102, 1000.00, "Jane Bloggs", "Dublin", "Personal", "Good"));
+     accountArray.add(new Account(103, 1000.00, "John Doe", "Waterford", "Corporate", "Good"));
+     NumOfAccounts = 4;
+    }
 	
-	public void createAccount(int a, double b, String c, String d, String e, String f)
-    {
+    public void createAccount(int a, double b, String c, String d, String e, String f) {
         accountArray.add(new Account(a, b, c, d, e, f));
         NumOfAccounts++;
     }
 
-	public void deleteAccount(int anAccNumber)
-    {
+    public void deleteAccount(int anAccNumber) {
 		Iterator<Account> iterator = accountArray.iterator();
 	
 		while(iterator.hasNext()) {
@@ -33,8 +31,7 @@ public class Bank_RealSubject implements ATM_Interface {
 		NumOfAccounts--;
     }
 
-    public void updateAccBalance(int anAccNumber, double newBalance)
-    {
+    public void updateAccBalance(int anAccNumber, double newBalance) {
     	Iterator<Account> iterator = accountArray.iterator();
    	
     	while(iterator.hasNext()) {
@@ -44,8 +41,7 @@ public class Bank_RealSubject implements ATM_Interface {
     	}
     }
 
-    public double getAccBalance(int anAccNumber)
-    {
+    public double getAccBalance(int anAccNumber) {
     	double balance = 0.00;
     	Iterator<Account> iterator = accountArray.iterator();
        	
